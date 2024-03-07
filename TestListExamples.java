632088895de97525205a0f0f4693c9893eabc75f
ayah825 <156377548+ayah825@users.noreply.github.com>
2024-03-07 14:14:25 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -10,12 +10,41 @@ class IsMoon implements StringChecker {
 }
 
 public class TestListExamples {
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
-    assertEquals(expected, merged);
+    assertEquals("Testing merge right end",expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b");
+    List<String> right = Arrays.asList("d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "b", "d");
+    assertEquals("Testing Merge left end", expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeSame() {
+    List<String> left = Arrays.asList("a", "a", "a", "a");
+    List<String> right = Arrays.asList("a", "a");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "a", "a", "a", "a");
+    assertEquals("Testing Merge same",expected, merged);
+  } 
+
+  @Test(timeout = 500)
+  public void testMergeMoon() {
+    List<String> left = Arrays.asList("a", "b", "moon");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "d", "moon");
+    assertEquals("Testing Merge moon",expected, merged);
+  } 
 }
+
