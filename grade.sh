ecc226ad4719afc723f76807ff66b6bd185b853d
ayah825 <156377548+ayah825@users.noreply.github.com>
2024-03-07 14:13:48 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,44 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+if [[ -f student-submission/ListExamples.java ]] 
+then 
+    cp student-submission/ListExamples.java grading-area/
+    echo "File Found"
+else 
+    echo "File ListExamples.java not found"
+    exit 1
+fi
+
+
+cp -r lib grading-area/
+
+cp TestListExamples.java grading-area/
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]] 
+then 
+    echo "The program failed to complile"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+TESTRESULTS=$(cat test-output.txt | tail -n 2 | head -n 1)
+
+if [[ $(echo $TESTRESULTS | awk -F '[ ]'  '{print $1}') == 'OK' ]]
+then
+    TESTSRUN=$(echo $TESTRESULTS | awk -F '[( ]'  '{print $3}')
+    FAILURES=0
+else
+    TESTSRUN=$(echo $TESTRESULTS | awk -F '[, ]'  '{print $3}')
+    FAILURES=$(echo $TESTRESULTS | awk -F '[, ]'  '{print $6}')
+fi
+
+SUCSESS=$(( TESTSRUN - FAILURES ))
+
+echo "Your score is: $SUCSESS / $TESTSRUN "
